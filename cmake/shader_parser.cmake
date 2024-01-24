354db8633b210b2a4bc259e8b05aaf6460ca033d
Andrew Naumov <andrejnau@gmail.com>
2024-01-24 11:14:01 +0100

    Use SHADER_* source properties over VS_SHADER_*

--- cmake/shader_parser.cmake
+++ cmake/shader_parser.cmake
@@ -6,9 +6,9 @@ function(gen_shaders_ref target shaders shaders_ref)
         get_filename_component(shader_name ${full_shader_path} NAME_WE)
         string(REPLACE "${assets_path}" "" shader_path ${full_shader_path})
         set(output_file ${output_dir}/${shader_name}.h)
-        get_property(entrypoint SOURCE ${full_shader_path} PROPERTY VS_SHADER_ENTRYPOINT)
-        get_property(type SOURCE ${full_shader_path} PROPERTY VS_SHADER_TYPE)
-        get_property(model SOURCE ${full_shader_path} PROPERTY VS_SHADER_MODEL)
+        get_property(entrypoint SOURCE ${full_shader_path} PROPERTY SHADER_ENTRYPOINT)
+        get_property(type SOURCE ${full_shader_path} PROPERTY SHADER_TYPE)
+        get_property(model SOURCE ${full_shader_path} PROPERTY SHADER_MODEL)
         add_custom_command(OUTPUT ${output_file}
             COMMAND ${CMAKE_COMMAND} -E echo ${assets_path} ${shader_name} ${shader_path} ${entrypoint} ${type} ${model} ${template} ${output_dir} ${build_folder}
             COMMAND ${CMAKE_COMMAND} -E make_directory ${output_dir}
