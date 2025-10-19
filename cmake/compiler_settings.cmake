3c01885f6a1860811831872998f5838ae253ca9c
Andrew Naumov <andrejnau@gmail.com>
2025-10-19 21:25:14 +0200

    Update compiler_settings

--- cmake/compiler_settings.cmake
+++ cmake/compiler_settings.cmake
@@ -1,23 +1,24 @@
-set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_COMPILE_WARNING_AS_ERROR OFF)
 
-if (MSVC)
-    add_compile_options("/MP")
-    add_compile_definitions(UNICODE _UNICODE)
-    add_compile_definitions(_CRT_SECURE_NO_WARNINGS)
-    add_compile_definitions(NOMINMAX)
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    add_compile_options(
-        $<$<COMPILE_LANGUAGE:CXX>:-Wno-array-bounds>
-        $<$<COMPILE_LANGUAGE:CXX>:-Wno-stringop-overflow>
-    )
-endif()
+set(appleclang_compile_options
+    -Wno-missing-template-arg-list-after-template-kw
+)
+set(msvc_compile_options
+    /MP
+)
+add_compile_options(
+    "$<$<COMPILE_LANG_AND_ID:CXX,AppleClang>:${appleclang_compile_options}>"
+    "$<$<COMPILE_LANG_AND_ID:CXX,MSVC>:${msvc_compile_options}>"
+)
 
-if (APPLE)
-    add_compile_options(
-        -Wno-missing-template-arg-list-after-template-kw
-    )
-    set(CMAKE_BUILD_WITH_INSTALL_RPATH ON)
-    set(CMAKE_INSTALL_RPATH "/usr/local/lib")
-endif()
+set(msvc_compile_definitions
+    _UNICODE
+    NOMINMAX
+    UNICODE
+)
+add_compile_definitions(
+    "$<$<COMPILE_LANG_AND_ID:CXX,MSVC>:${msvc_compile_definitions}>"
+)
